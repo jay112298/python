e25762bc0937ec29173a13679ac199f7813053d9
Jitendra Asole <jasole1111@gmail.com>
2022-12-24 13:23:45 +0530

    Dummy commit

--- Day_1_jitendraAsole_24-12-2022.py
+++ Day_1_jitendraAsole_24-12-2022.py
@@ -1,19 +1,34 @@
 print("Today is 24th of December and it is sunny day with cold temperatures")
+# Addition problems
 print(2+1)
 print(65.36+45.21)
 print(3.66+6.25)
 print(5+10)
 print(20+60)
 print(60+40)
+
+# Substraction statements
 print(40-23)
 print(56.12-41.32)
 print(645.23697-123.567489)
+
+# Multiplication statements
 print(56*23)
 print(56.23*12.23)
+
+# Division statements
 print(56/56)
 print(12/2)
+
+# Floor division statements
 print(9//8)
+
+# Modulus statements
 print(100%9)
+
+# Exponential statements
 print(12**2)
 print(12**3)
+
+# Exponential square root
 print(36**0.5)
