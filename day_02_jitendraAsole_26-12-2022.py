bcee444eb86266b6f25776454f0efd37d78b6ca1
Jay Asole <jasole1111@gmail.com>
2022-12-26 11:32:22 +0530

    Variables assigned

--- day_02_jitendraAsole_26-12-2022.py
+++ day_02_jitendraAsole_26-12-2022.py
@@ -4,6 +4,12 @@ print(5**4)
 
 #Bodmas rule for calculating
 
-a = (2+10+20) * (2+5+9)
+print((2+10+20) * (2+5+9))
+print((2+20) * (2+5+9))
 
-print(a)
+#Variable assignment 
+
+a = 5
+b = 10
+c = 20.60
+d = 100.1011
