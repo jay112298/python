baa81fd04e3c3d09a0128686eada5172aa8846a8
Jay Asole <jasole1111@gmail.com>
2022-12-29 13:22:23 +0530

    Add list comprehension and modify day 5

--- day_05_jitendraAsole_27-12-2022.py
+++ day_05_jitendraAsole_27-12-2022.py
@@ -10,3 +10,83 @@ x.add(2)
 print(x)
 x.add(3)
 print(x)
+
+#List with repeated items
+
+int_list = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+print(set(int_list))
+
+print(int_list)
+
+#Boolean, set a boolean value to true
+
+bool = True
+
+bool = None
+
+print(bool)
+
+#Operators
+
+a = 3 
+b = 4 
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+#Number conditionals
+
+print(2 > 4)
+print(2 < 4)
+print(2 == 4)
+print(2 <= 4)
+print(2 >= 4)
+
+#If Else
+
+loc = 'Bank'
+
+if loc == 'Auto Shop':
+    print('Welcome to the auto shop')
+elif loc == 'Bank':
+    print('Welcome to the bank')
+else:
+    print('Where are you?')
+
+
+# Check if else using 2 numbers
+
+a = 55
+b = 6
+
+if a>b :
+    print("A is greater than B")
+else :
+    print("A is less than B")
+
+numA = int(input("Please enter the value of A: "))
+numB = int(input("Please enter the value of B: "))
+
+
+if numA>numB :
+    print("A is greater than B")
+else :
+    print("A is less than B")
+
+#Also check if they are equal
+
+numA = int(input("Please enter the value of A: "))
+numB = int(input("Please enter the value of B: "))
+
+
+if numA>numB :
+    print("A is greater than B")
+elif numA == numB :
+    print("A is equal to B")
+else :
+    print("A is less than B")
+
---
+++ listComprehension.py
@@ -0,0 +1,7 @@
+x = [l for l in range(1, 4)]
+
+print(x)
+
+matrix = [[j for j in range(1,4)] for i in range(1, 4)]
+
+print(matrix)
